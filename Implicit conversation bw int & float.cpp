9de20a0a2de62b944ea219a41784b9052c54b4a1
shaikshabbirali <124281233+shaikshabbirali@users.noreply.github.com>
2024-06-06 09:33:38 +0530

    Create Implicit conversation bw int & float.cpp

---
+++ Implicit conversation bw int & float.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int integerNumber = 10;
+    float floatNumber;
+
+    floatNumber = integerNumber;
+
+    std::cout << "Integer Number: " << integerNumber << std::endl;
+    std::cout << "Float Number: " << floatNumber << std::endl;
+
+    return 0;
+}
