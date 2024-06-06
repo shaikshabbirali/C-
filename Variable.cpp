226a92e155fd8ca0fc460ed0544a091c9ffc3f56
shaikshabbirali <124281233+shaikshabbirali@users.noreply.github.com>
2024-06-06 09:43:13 +0530

    Create Variable.cpp

---
+++ Variable.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int var1, var2, var3;
+
+    std::cout << "Address of var1: " << &var1 << std::endl;
+    std::cout << "Address of var2: " << &var2 << std::endl;
+    std::cout << "Address of var3: " << &var3 << std::endl;
+
+    return 0;
+}
