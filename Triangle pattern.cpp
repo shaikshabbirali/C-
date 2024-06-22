fe83dbebd5e94d6780e49ebe5a6931338378e1b4
shaikshabbirali <124281233+shaikshabbirali@users.noreply.github.com>
2024-06-22 10:32:39 +0530

    Create Triangle pattern.cpp

---
+++ Triangle pattern.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    int rows;
+
+    std::cout << "Enter the number of rows: ";
+    std::cin >> rows;
+
+    for (int i = 1; i <= rows; ++i)   {
+     
+        for (int j = i; j < rows; ++j) {
+            std::cout << " ";
+        }
+               for (int k = 1; k <= (2 * i - 1); ++k) {
+            std::cout << "*";
+        }
+                std::cout << std::endl;
+    }
+
+    return 0;
+}
